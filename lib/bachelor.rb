7ceea65e95a74b5e806254b57d06d9434cbd7e8a
Austin Luft <ausluft@gmail.com>
2018-09-26 01:01:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,9 +51,7 @@ end
 def get_average_age_for_season(data, season)
 ages = []
 age_total = 0
-data.each do |season, contestants|
-    contestants.each do |contestant|
-      contestant.each do |key, value|
+data.each do |season|
         if key == "age"
           ages.push(value)
           age_total = age_total + value.to_i
