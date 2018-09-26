297a98426dcd5eb97b2c31b86deef8054360c68c
Austin Luft <ausluft@gmail.com>
2018-09-26 01:22:15 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,66 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = []
+  data[season].each do |contestant|
+    contestant.each do |key, value|
+    if value == "Winner"
+      first_name = contestant["name"].split(" ")
+    end
+  end
+end
+    first_name[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |key, value|
+      if value == occupation
+        return contestant["name"]
+      end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  residents = []
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |key, value|
+      if value == hometown
+        residents.push(contestant["name"])
+      end
+    end
+  end
+end
+return residents.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      contestant.each do |key, value|
+      if value == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
+end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+ages = []
+age_total = 0
+  data[season].each do |array_of_contestants|
+  array_of_contestants.each do |key, value|
+        if key == "age"
+          ages.push(value)
+          age_total = age_total + value.to_i
+        end
+  end
+  end
+return (age_total.to_f / ages.length.to_f).round
 end
+
+
+
